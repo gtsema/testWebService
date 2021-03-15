8581b759c1b6401b54c261c6bc0fa98c6ac4ba8e
gtsema <42067282+gtsema@users.noreply.github.com>
2021-03-15 13:08:16 +0300

    Update Application.java

--- src/main/java/ru/example/Application.java
+++ src/main/java/ru/example/Application.java
@@ -3,7 +3,7 @@ package ru.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
